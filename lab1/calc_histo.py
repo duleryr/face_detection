8a5423b435ab9bb901d37e9d717afc421f56bfec
Romain Duléry <dulery.r@gmail.com>
2017-10-18 11:48:46 +0200

    changement identation 4 char'

--- lab1/calc_histo.py
+++ lab1/calc_histo.py
@@ -13,32 +13,31 @@ img_info = parse_file.get_img_info(descriptor_file)
 #load the image
 img = cv2.imread(img_info.img_path)
 #DEBUG
-cv2.imshow('face', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 #creating a mask for the histogram
 mask = img.copy()
 mask[:] = (0, 0, 0)
 
 for i in range(0, img_info.nb_faces):
-	e_tmp = img_info.list_ellipse[i]
-	cv2.ellipse(mask,(int(e_tmp.c_x),int(e_tmp.c_y)),(int(e_tmp.r_a),
+    e_tmp = img_info.list_ellipse[i]
+    cv2.ellipse(mask,(int(e_tmp.c_x),int(e_tmp.c_y)),(int(e_tmp.r_a),
 		int(e_tmp.r_b)),int(e_tmp.theta),0,360,(255, 255, 255), -1)
 #DEBUG
 masked_img = cv2.bitwise_and(img, img, mask = mask[:,:,0])
 
 color = {"b","g","r"}
 
-hist_total = []
-for i,col in enumerate(color): #enumerate returns always a couple, e.g.(0,'r')
-	hist = cv2.calcHist([img],[i], mask[:,:,0], [256], [0,256])
-	hist_total.append(hist)
-	plt.plot(hist, color = col)
-	plt.xlim([0,256])
-plt.show()
-
+def calcHist():
+    hist_total = []
+    for i,col in enumerate(color): #enumerate returns always a couple, e.g.(0,'r')
+    	hist = cv2.calcHist([img],[i], mask[:,:,0], [256], [0,256])
+    	hist_total.append(hist)
+    	plt.plot(hist, color = col)
+    	plt.xlim([0,256])
+    return hist
 
 #TODO: accumulate histograms for more than one image
 
-
+cv2.imshow('face', masked_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- lab1/draw_ellipse.py
+++ lab1/draw_ellipse.py
@@ -13,9 +13,10 @@ img_info = parse_file.get_img_info(descriptor_file)
 img = cv2.imread(img_info.img_path)
 
 for i in range(0, img_info.nb_faces):
-	e_tmp = img_info.list_ellipse[i]
-	cv2.ellipse(img,(int(e_tmp.c_x),int(e_tmp.c_y)),(int(e_tmp.r_a),
-		int(e_tmp.r_b)),int(e_tmp.theta),0,360,(0, 255, 0), 3)
+    e_tmp = img_info.list_ellipse[i]
+    cv2.ellipse(img,(int(e_tmp.c_x),int(e_tmp.c_y)),(int(e_tmp.r_a),
+    	int(e_tmp.r_b)),int(e_tmp.theta),0,360,(0, 255, 0), 3)
+
 cv2.imshow("face", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
