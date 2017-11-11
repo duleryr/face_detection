b9802a11a8e990d7dcf79c8168d69c29c23786ee
Felix Hahnlein <felix.hahnlein@grenoble-inp.org>
2017-11-11 14:13:13 +0100

    parallelization

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -9,6 +9,15 @@ from matplotlib import pyplot as plt
 from face_detection import get_statistics_one_image
 import pickle
 import graphical_tools
+import multiprocessing
+import time
+
+class s_vec:
+    def __init__(self, tp_vec, fp_vec, tn_vec, fn_vec):
+        self.tp_vec = tp_vec
+        self.fp_vec = fp_vec
+        self.tn_vec = tn_vec
+        self.fn_vec = fn_vec
 
 """ Initialization : Choice of the files used for the training/testing """
 try:
@@ -76,26 +85,70 @@ graphical_tools.plot_3d_color_histogram(lookup_table_data, n_quantification)
 
 print("Face detection...")
 
-bias_vec = np.arange(-0.5, 0.6, 0.05)
+bias_vec = np.arange(-0.5, 0.55, 0.05)
 
 # per-image operations
 tpr_vec, fpr_vec, tp_vec, fp_vec, tn_vec, fn_vec = ([0]*len(bias_vec) for i in range(6))
 
+
+#Parallel processing
+img_info_vec = []
 for k in range(nb_images_testing):
-    print(" "+str(k)+"-th image")
+    img_info_vec.append(parse_file.get_img_info(fd))
 
-    img_info = parse_file.get_img_info(fd)
-    img = cv2.imread(img_info.img_path)
-
-    print("  bias = ", end = "", flush = True)
+results = multiprocessing.Array('i', 4*len(bias_vec), lock=False)
+def parallel_img(pid):
+    print(" "+str(pid)+"-th image")
+    img_info_local = img_info_vec[pid]
+    img = cv2.imread(img_info_local.img_path)
+    #print("  bias = ", end = "", flush = True)
     for i,bias in enumerate(bias_vec):
-        print(bias, end = ", ", flush = True)
-        s = get_statistics_one_image(lookup_table_data,img,img_info,bias,4,4,9,9, n_quantification)
-        tp_vec[i] += s.tp
-        fp_vec[i] += s.fp
-        tn_vec[i] += s.tn
-        fn_vec[i] += s.fn
-    print("done.")
+        #print(bias, end = ", ", flush = True)
+        s = get_statistics_one_image(lookup_table_data,img,img_info_local,bias,4,4,9,9, n_quantification)
+        results[i*4+0] += s.tp
+        results[i*4+1] += s.fp
+        results[i*4+2] += s.tn
+        results[i*4+3] += s.fn
+    #print("done.")
+    return 0
+
+p = multiprocessing.Pool(multiprocessing.cpu_count())
+
+start_time = time.time()
+p.map(parallel_img, range(nb_images_testing))
+end_time = time.time()
+print("Parallel time=", end_time - start_time)
+#for i in results:
+#    print(i)
+
+#transfer results to stats arrays
+for i in range(len(bias_vec)):
+    tp_vec[i] = results[i*4+0]
+    fp_vec[i] = results[i*4+1]
+    tn_vec[i] = results[i*4+2]
+    fn_vec[i] = results[i*4+3]
+
+
+
+# Serialized processing
+#start_time = time.time()
+#for k in range(nb_images_testing):
+#    print(" "+str(k)+"-th image")
+#
+#    img_info = parse_file.get_img_info(fd)
+#    img = cv2.imread(img_info.img_path)
+#
+#    print("  bias = ", end = "", flush = True)
+#    for i,bias in enumerate(bias_vec):
+#        print(bias, end = ", ", flush = True)
+#        s = get_statistics_one_image(lookup_table_data,img,img_info,bias,4,4,9,9, n_quantification)
+#        tp_vec[i] += s.tp
+#        fp_vec[i] += s.fp
+#        tn_vec[i] += s.tn
+#        fn_vec[i] += s.fn
+#    print("done.")
+#end_time = time.time()
+#print("Serial time=", end_time - start_time)
 
 # calculating the tpr, fpr
 for i in range(len(bias_vec)):
