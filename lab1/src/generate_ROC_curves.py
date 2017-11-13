ff345e81e2c485dd9a514117a58c45719c15759d
Romain Duléry <dulery.r@gmail.com>
2017-11-13 21:32:49 +0100

    update generateROCcurve pour tester

--- lab1/src/generate_ROC_curves.py
+++ lab1/src/generate_ROC_curves.py
@@ -1,5 +1,10 @@
 import main_testing
 import os
 
+# Tests
 for lookup_table in os.listdir("../lookup_tables"):
-    main_testing.main("10", 100, "../lookup_tables/" + lookup_table)
+    main_testing.main("10", 2, "../lookup_tables/" + lookup_table)
+
+# C'est parti
+for lookup_table in os.listdir("../lookup_tables"):
+    main_testing.main("10", 250, "../lookup_tables/" + lookup_table)
