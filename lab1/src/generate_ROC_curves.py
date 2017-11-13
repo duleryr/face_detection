e024b7f7238b2caacca5da87c40290e90c5ead8d
Felix Hahnlein <felix.hahnlein@grenoble-inp.org>
2017-11-13 22:02:34 +0100

    modified roc-script

--- lab1/src/generate_ROC_curves.py
+++ lab1/src/generate_ROC_curves.py
@@ -2,4 +2,4 @@ import main_testing
 import os
 
 for lookup_table in os.listdir("../lookup_tables"):
-    main_testing.main("10", 100, "../lookup_tables/" + lookup_table)
+    os.system("python3 main_testing.py 10 2 ../lookup_tables/"+str(lookup_table))
