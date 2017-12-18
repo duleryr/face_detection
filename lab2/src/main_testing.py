41ff57bbb517ee62d076902360ccdb5f2b8fe248
Felix Hahnlein <felix.hahnlein@grenoble-inp.org>
2017-12-18 21:29:59 +0100

    precision-recall curves loop

--- lab2/src/main_testing.py
+++ lab2/src/main_testing.py
@@ -11,6 +11,7 @@ import time
 import graphical_tools
 import single_image_evaluation
 from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import auc
 
 if __name__ == '__main__':
     """ Initialization : Choice of the files used for the training/testing """
@@ -40,90 +41,109 @@ if __name__ == '__main__':
     if debug:
         print("Face detection...")
 
-    scale_factor=1.05 # later we will run through these parameters
+    scale_factor=1.1 # later we will run through these parameters
     min_neighbours=3
 
-    #for scale_factor in np.arange(1.1,2.0,0.1):
-    #    for min_neighbours in range(0,10,1):
-    print("scale_factor: "+str(scale_factor)+", min_neighbours: "+str(min_neighbours))
-    y_true = []
-    levelWeights_all=[]
-    counter = 0
-    FN = 0
-    for img_info in img_info_vec:
-        counter += 1
-        #print("img nb: "+str(counter))
-        # viola-jones works with grayscale images
-        gray_img = cv2.imread(img_info.img_path,0) # 0 = IMREAD_GRAYSCALE
-        [detected_faces,rejectLevels,levelWeights] = face_cascade.detectMultiScale3(gray_img,scale_factor,min_neighbours,outputRejectLevels=True)
-        img_info.img_shape = gray_img.shape
-        y_true_tmp = single_image_evaluation.evaluate(img_info,detected_faces)
-        y_true.append(y_true_tmp)
-        FN += len(img_info.list_ellipse)
-        if(len(levelWeights)>0):
-            levelWeights = np.concatenate(levelWeights)
-            levelWeights_all.append(levelWeights)
+    plt_handle_vec = []
+    for scale_factor in np.arange(1.1,2.0,0.05):
+        for min_neighbours in range(1,2,1):
+            print("scale_factor: "+str(scale_factor)+", min_neighbours: "+str(min_neighbours))
+            y_true = []
+            levelWeights_all=[]
+            counter = 0
+            FN = 0
+            for img_info in img_info_vec:
+                counter += 1
+                #print("img nb: "+str(counter))
+                # viola-jones works with grayscale images
+                gray_img = cv2.imread(img_info.img_path,0) # 0 = IMREAD_GRAYSCALE
+                [detected_faces,rejectLevels,levelWeights] = face_cascade.detectMultiScale3(gray_img,scale_factor,min_neighbours,outputRejectLevels=True)
+                img_info.img_shape = gray_img.shape
+                y_true_tmp = single_image_evaluation.evaluate(img_info,detected_faces)
+                y_true.append(y_true_tmp)
+                FN += len(img_info.list_ellipse)
+                if(len(levelWeights)>0):
+                    levelWeights = np.concatenate(levelWeights)
+                    levelWeights_all.append(levelWeights)
 
-    y_true = np.concatenate(y_true)
-    levelWeights_all = np.concatenate(levelWeights_all)
-    FN = FN-y_true.sum()
-    weight_tp_fp_vec = zip(levelWeights_all,y_true)
-    levelWeights_all,y_true = zip(*sorted(weight_tp_fp_vec))
-#    precision,recall, thresholds = precision_recall_curve(y_true,levelWeights_all,1)
-    precision = []
-    recall = []
-    TP = 0
-    FP = 0
-    P = 0
-    R = 0
-    y_true = np.array(y_true)
-    #print(y_true)
-    new_TP = [y_true.sum()]
-    new_FP = [len(y_true)-y_true.sum()]
-    new_FN = [FN]
-    # ============= STRATEGIE GROUPE 2 ================
-    # ici on fait augmenter le seuil
-    for i in range(len(y_true)):
-        # DEBUG
-        #print("TP : "+str(new_TP[i])+", FP: "+str(new_FP[i])+", FN: "+str(new_FN[i]))
-        if(y_true[i]==1):
-            new_TP.append(new_TP[i]-1)
-            new_FN.append(new_FN[i]+1)
-            new_FP.append(new_FP[i])
-        else:
-            new_FP.append(new_FP[i]-1)
-            new_TP.append(new_TP[i])
-            new_FN.append(new_FN[i])
-    # compute precision, recall
-    for i in range(len(new_TP)):
-        if((new_TP[i]+new_FP[i])!=0):
-            P = float(new_TP[i])/float(new_TP[i]+new_FP[i])
-            R = float(new_TP[i])/float(new_TP[i]+new_FN[i])
-            precision.append(P)
-            recall.append(R)
-#    print(precision)
-#    print(recall)
-        
-    # ========== OLD STRATEGY ===============
-#    for i in range(len(y_true)-1,0,-1):
-#        if(y_true[i]==1):
-#            TP += 1
-#            #FN -= 1
-#        else:
-#            FP += 1
-#        print("TP: "+str(TP)+", FP: "+str(FP)+", FN: "+str(FN))
-#        P = float(TP)/float(TP+FP)
-#        R = float(TP)/float(FN)
-#        precision.append(P)
-#        recall.append(R)
-#    print(precision)
-#    print(recall)
+            y_true = np.concatenate(y_true)
+            levelWeights_all = np.concatenate(levelWeights_all)
+            FN = FN-y_true.sum()
+            weight_tp_fp_vec = zip(levelWeights_all,y_true)
+            levelWeights_all,y_true = zip(*sorted(weight_tp_fp_vec))
+#            precision,recall, thresholds = precision_recall_curve(y_true,levelWeights_all,1)
+            precision = []
+            recall = []
+            TP = 0
+            FP = 0
+            P = 0
+            R = 0
+            y_true = np.array(y_true)
+#            print(y_true)
+#            print(levelWeights_all)
+            new_TP = [y_true.sum()]
+            new_FP = [len(y_true)-y_true.sum()]
+            new_FN = [FN]
+            # ============= STRATEGIE GROUPE 2 ================
+            # ici on fait augmenter le seuil
+#            print("TP : "+str(new_TP[0])+", FP: "+str(new_FP[0])+", FN: "+str(new_FN[0]))
+            for i in range(len(y_true)):
+                # DEBUG
+                if(y_true[i]==1):
+                    new_TP.append(new_TP[i]-1)
+                    new_FN.append(new_FN[i]+1)
+                    new_FP.append(new_FP[i])
+                else:
+                    new_FP.append(new_FP[i]-1)
+                    new_TP.append(new_TP[i])
+                    new_FN.append(new_FN[i])
+                # DEBUG
+#                print("STEP "+str(i))
+#                print("TP : "+str(new_TP[i])+", FP: "+str(new_FP[i])+", FN: "+str(new_FN[i]))
+#                P = float(new_TP[i])/float(new_TP[i]+new_FP[i])
+#                R = float(new_TP[i])/float(new_TP[i]+new_FN[i])
+#                print("Precision: "+str(P)+", Recall: "+str(R))
+#                print("")
+            # compute precision, recall
+            precision.append(0)
+            recall.append(1)
+            for i in range(len(new_TP)):
+                if((new_TP[i]+new_FP[i])!=0):
+                    P = float(new_TP[i])/float(new_TP[i]+new_FP[i])
+                    R = float(new_TP[i])/float(new_TP[i]+new_FN[i])
+                    precision.append(P)
+                    recall.append(R)
+            precision.append(1)
+            recall.append(0)
+#            print(precision)
+#            print(recall)
+            print(auc(recall,precision))
+                
+            # ========== OLD STRATEGY ===============
+#            for i in range(len(y_true)-1,0,-1):
+#                if(y_true[i]==1):
+#                    TP += 1
+#                    #FN -= 1
+#                else:
+#                    FP += 1
+#                print("TP: "+str(TP)+", FP: "+str(FP)+", FN: "+str(FN))
+#                P = float(TP)/float(TP+FP)
+#                R = float(TP)/float(FN)
+#                precision.append(P)
+#                recall.append(R)
+#            print(precision)
+#            print(recall)
 
-    plt.gcf().clear()
-    plt.step(recall, precision, color='b', alpha=0.2, where='post')
-    plt.title("scaleFactor: "+str(scale_factor)+", minNeighbours: "+str(min_neighbours))
+#            plt.gcf().clear()
+#            plt.plot(recall,precision)
+            plt.step(recall, precision, where='post', label="scaleFactor: "+str(scale_factor)+", AUC: "+str(np.around(auc(recall,precision),decimals=5)))
+#    plt.title("scaleFactor: "+str(scale_factor)+", minNeighbours: "+str(min_neighbours))
+    plt.title("minNeighbours: "+str(min_neighbours))
     plt.axis([0,1.1,0,1.1])
     plt.xlabel("Recall")
     plt.ylabel("Precision")
-    #plt.savefig("figures/"+str(scale_factor)+"_"+str(min_neighbours)+".png")
-    plt.show()
+    plt.legend()
+    fig = plt.gcf()
+    fig.set_size_inches((16, 10), forward=False)
+    fig.savefig("figures/"+str(min_neighbours)+".png",dpi=600)
+    #plt.show()
